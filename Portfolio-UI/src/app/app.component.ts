f38adf29365a91e9ca8c7a1d058d62fc05d57acf
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-01-14 16:31:32 +0300

    setting up the form

--- Portfolio-UI/src/app/app.component.ts
+++ Portfolio-UI/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio-UI';
+
+  // Form declaration
+  contactForm !: FormGroup;
+
+  constructor(private fbService : FormBuilder) { };
+
+  ngOnInit() { 
+
+  }
+
+  // Submit form function
+  sendMessage() {
+    console.log(this.contactForm.value)
+  }
+
+
+
 }
