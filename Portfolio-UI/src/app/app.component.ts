36d1b72657be290b52570a7f852f15821effe4af
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-01-14 16:37:41 +0300

    creating contact me form model

--- Portfolio-UI/src/app/app.component.ts
+++ Portfolio-UI/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +12,22 @@ export class AppComponent implements OnInit {
   // Form declaration
   contactForm !: FormGroup;
 
+    // Email validation expression
+  emailRegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+
+  // Phone number validation expression
+  phoneRegExp = /^(?:254|\+254|0)?(7(?:(?:[129][0–9])|(?:0[0–8])|(4[0–1]))[0–9]{6})$/
+
   constructor(private fbService : FormBuilder) { };
 
   ngOnInit() { 
+
+    this.contactForm = this.fbService.group({
+      names: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', Validators.required, Validators.pattern(this.emailRegExp)],
+      phonenumber: ['', Validators.pattern(this.phoneRegExp)],
+      message:['', Validators.required]
+    })
 
   }
 
