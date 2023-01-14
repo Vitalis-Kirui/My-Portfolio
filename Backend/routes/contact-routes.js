7a7381d95c04d3f0dea090e308d4f2710a5923d6
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-01-14 19:18:52 +0300

    creating contact me routes

---
+++ Backend/routes/contact-routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const contactcontrollers = require('../controllers/contact-controllers');
+
+// creating a new message
+router.post('/send-message', contactcontrollers.contactme);
+
+// getting all messages
+router.get('/get-messages', contactcontrollers.getallmessages);
+
+// get a message by id
+router.get('/get-message/:id', contactcontrollers.getsinglemessage);
+
+// delete a message by id
+router.delete('/delete-message/:id', contactcontrollers.deletemessage);
+
+// Get unread messages
+router.get('/get-unread-messages', contactcontrollers.getunreadmessages);
+
+module.exports = router;
